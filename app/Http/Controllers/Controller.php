898439b974631c5fbb367ecd5ab109f18a0268b6
David Cobas <cobasesp.developer@gmail.com>
2020-12-07 12:31:13 +0100

    Funcion en controller paa devolver json en utf-8

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,5 +6,9 @@ use Laravel\Lumen\Routing\Controller as BaseController;
 
 class Controller extends BaseController
 {
-    //
+    protected function jsonResponse($data, $code = 200)
+    {
+        return response()->json($data, $code,
+            ['Content-Type' => 'application/json;charset=UTF-8', 'Charset' => 'utf-8'], JSON_UNESCAPED_UNICODE);
+    }
 }
